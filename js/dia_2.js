1f6666c071a9d2af653adde141f607a974a53b9f
AbelIbarraSol <abelibarrasol18@gmail.com>
2025-02-26 23:16:49 -0500

    Ejercicio 2 Funcional

--- js/dia_2.js
+++ js/dia_2.js
@@ -1,21 +1,20 @@
-function submit__Day2(){
+function submit__Day2() {
     let input_Name = document.getElementById("input-Name").value;
     let input_Age = document.getElementById("input-Age").value;
     let input_Lang = document.getElementById("input-Lang").value;
-    let btnSubmit = document.getElementById("btn-submit");
+    let btnSubmit = document.getElementById("btn-Submit");
 
     let contentMessage = document.getElementById("message-content");
     let contentButtons = document.querySelector(".day-2__button-content");
 
-   contentMessage.innerHTML = "";
-   contentButtons.innerHTML = ""; // Limpiar los botones antes de crear uno nuevo
+    contentMessage.innerHTML = "";
 
     if (input_Name === "") {
         alert('Tienes que ingresar tu nombre');
         return;
     }
-    
-    if(input_Age === "") {
+
+    if (input_Age === "") {
         alert('Tienes que ingresar tu edad');
         return;
     }
@@ -25,23 +24,22 @@ function submit__Day2(){
         return;
     }
 
-    //Crear elemento mensaje
+    // Crear elemento mensaje
     let message = document.createElement("h3");
     message.textContent = `Hola ${input_Name}, tienes ${input_Age} años y ya estás aprendiendo ${input_Lang}!`;
     message.classList.add("day-2__text");
 
-    //Crear elemento boton limpiar
+    // Crear elemento botón limpiar
     let cleanContent = document.createElement("button");
     cleanContent.textContent = "Limpiar";
     cleanContent.classList.add("day-2__button", "day-2__button--clean");
 
-    // Asignamos evento on click al boton limpiar, usando arrow function ()=>
-    cleanContent.onclick =  () => {
-        cleanElements(cleanContent, btnSubmit); //pasar boton como argumento
+    // Asignar evento on click al botón limpiar
+    cleanContent.onclick = () => {
+        cleanElements(cleanContent); // pasar botón como argumento
     }
-    
 
-    //Insertar elemento hijo en contenedor padre
+    // Insertar elemento hijo en contenedor padre
     contentMessage.appendChild(message);
     contentButtons.appendChild(cleanContent);
 
@@ -49,15 +47,14 @@ function submit__Day2(){
     btnSubmit.disabled = true;
 }
 
-function cleanElements(cleanButton, submitButton) {  
-    let btnSubmit = document.getElementById("btn-submit"); 
+function cleanElements(cleanButton) {
     document.getElementById("input-Name").value = "";
     document.getElementById("input-Age").value = "";
     document.getElementById("input-Lang").value = "";
     document.getElementById("message-content").innerHTML = "";
-    //Eliminar el boton del DOM
+    // Eliminar el botón "Limpiar" del DOM
     cleanButton.remove();
 
     // Habilitar el botón "Enviar"
-    submitButton.disabled = false;
-}   
+    document.getElementById("btn-Submit").disabled = false;
+}
